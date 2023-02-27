4105326fa968bec8fc6470ec6486b7a6db99e88a
Landon <chura.pyon@gmail.com>
2023-02-27 19:27:46 +0000

    Refactored code per Claire's feedback

--- RB101/lesson-2/loan-calculator/loan.rb
+++ RB101/lesson-2/loan-calculator/loan.rb
@@ -1,5 +1,10 @@
+require 'yaml'
+
 RESPONSES = %w(y yes n no)
 AFFIRMATIVE = %w(y yes)
+MONTHS_PER_YEAR = 12
+
+MESSAGES = YAML.load_file("loan.yaml")
 
 def prompt(message)
   puts "=> #{message}"
@@ -9,13 +14,12 @@ def get_loan_amt
   loan_amt = ''
 
   loop do
-    prompt("Please enter the amount of the loan:")
+    prompt(MESSAGES["enter_amount"])
     loan_amt = gets.chomp
 
     break if valid_loan_amt?(loan_amt)
 
-    prompt("Must enter a positive non-zero number for loan amount. " \
-           "Please try again.")
+    prompt(MESSAGES["invalid_amount"])
   end
 
   loan_amt.to_f
@@ -29,14 +33,13 @@ def get_apr
   rate = ''
 
   loop do
-    prompt("Please enter the Annual Percentage Rate (APR) of the loan " \
-           "as a percentage:")
-    prompt("e.g. if your APR is 4.5% then you would enter 4.5 and not .045")
+    prompt(MESSAGES["enter_apr"])
+    prompt(MESSAGES["example_apr"])
     rate = gets.chomp
 
     break if valid_apr?(rate)
 
-    prompt("Must enter a number between 0 and 30 inclusive. Please try again.")
+    prompt(MESSAGES["invalid_apr"])
   end
 
   rate.to_f
@@ -78,56 +81,80 @@ def valid_percentage?(number)
 end
 
 def get_loan_duration
-  loan_dur = ''
-
+  loan_dur = Hash.new(2)
+  
   loop do
-    prompt("Please enter the loan duration rounded to the nearest half a year:")
-    prompt("e.g. 20 and 20.5 are acceptable entries, but 15.3 or 17.9 are not.")
-    loan_dur = gets.chomp
+    loan_dur[:years] = get_loan_duration_years
+    loan_dur[:months] = get_loan_duration_months
 
-    break if valid_duration?(loan_dur)
+    break unless both_zero?(loan_dur)
 
-    prompt("Must be a positive non-zero number rounded to the nearest half " \
-           "year.  Please try again.")
+    prompt(MESSAGES["invalid_loan_duration"])
   end
 
-  loan_dur.to_f
+  loan_dur
 end
 
-=begin
-The regex in the following method looks for a string that contains the
-following:
-  - 1 or more digits at the beginning of the string
-  - followed by an optional period
-  - followed by an optional 5
-  - followed by zero or more 0's at the end of the string
-=end
+def get_loan_duration_years
+  years = ''
 
-def valid_duration?(num_string)
-  /^\d+\.?5?0*$/.match?(num_string) && !num_string.to_f.zero?
+  loop do
+    prompt(MESSAGES["enter_years"])
+    prompt(MESSAGES["example_year"])
+    years = gets.chomp
+
+    break if valid_integer?(years)
+
+    prompt(MESSAGES["invalid_year_month"])
+  end
+
+  years.to_i
+end
+
+def get_loan_duration_months
+  months = ''
+
+  loop do
+    prompt(MESSAGES["enter_months"])
+    prompt(MESSAGES["example_month"])
+    months = gets.chomp
+
+    break if valid_integer?(months)
+
+    prompt(MESSAGES["invalid_year_month"])
+  end
+
+  months.to_i
+end
+
+def both_zero?(num_array)
+  num_array[:years].to_i.zero? && num_array[:months].to_i.zero?
 end
 
 def calc_monthly_payment(loan_amt, monthly_rate, loan_dur_months)
-  loan_amt * (monthly_rate / (1 - (1 + monthly_rate)**(-loan_dur_months)))
+  if monthly_rate.zero?
+    loan_amt / loan_dur_months
+  else
+    loan_amt * (monthly_rate / (1 - (1 + monthly_rate)**(-loan_dur_months)))
+  end
 end
 
 def get_do_again_response
   answer = ''
 
   loop do
-    prompt("Would you like to calculate another loan payment? " \
-           "(y for yes, n for no)")
+    prompt(MESSAGES["do_again"])
     answer = gets.chomp.downcase
 
-    break if valid_do_again_response(answer)
+    break if valid_do_again_response?(answer)
 
-    prompt("Must enter y or n for your response.  Please try again.")
+    prompt(MESSAGES["invalid_do_again"])
   end
 
   answer
 end
 
-def valid_do_again_response(string)
+def valid_do_again_response?(string)
   RESPONSES.include?(string)
 end
 
@@ -138,25 +165,28 @@ end
 # Main
 
 system("clear") || system("cls")
-prompt("Welcome to the Mortgage and Auto Loan Calculator!")
+prompt(MESSAGES["welcome"])
 
 loop do
   loan_amount = get_loan_amt
   apr = get_apr / 100
-  loan_duration_in_years = get_loan_duration
+  loan_duration = get_loan_duration
 
-  monthly_interest_rate = apr / 12
-  loan_duration_in_months = loan_duration_in_years * 12
+  monthly_interest_rate = apr / MONTHS_PER_YEAR
+  loan_duration_in_months = loan_duration[:years] * MONTHS_PER_YEAR +
+                            loan_duration[:months]
 
   monthly_payment = calc_monthly_payment(loan_amount,
                                          monthly_interest_rate,
                                          loan_duration_in_months)
   monthly_payment = format('%.2f', monthly_payment)
+  years = loan_duration_in_months / MONTHS_PER_YEAR
+  months = loan_duration_in_months % MONTHS_PER_YEAR
 
-  prompt("With a loan amount of $#{loan_amount},")
-  prompt("And an APR of #{apr * 100}%,")
-  prompt("And a loan duration of #{loan_duration_in_years} years,")
-  prompt("Your monthly payment will be $#{monthly_payment}.")
+  prompt(MESSAGES["loan_amount"] + "$#{loan_amount},")
+  prompt(MESSAGES["apr"] + "#{apr * 100}%,")
+  prompt(format(MESSAGES["loan_duration"], years: years, months: months))
+  prompt(MESSAGES["monthly_payment"] + "$#{monthly_payment}.")
 
   answer = get_do_again_response
 
@@ -165,4 +195,4 @@ loop do
   system("clear") || system("cls")
 end
 
-prompt("Thank you for using the Mortgage and Auto Loan Calculator. Goodbye.")
+prompt(MESSAGES["goodbye"])
