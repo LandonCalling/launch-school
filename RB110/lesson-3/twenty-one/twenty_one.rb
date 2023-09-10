5ef9d5032f38ac8246ba1526e912310369567fac
Landon Hill <52553841+LandonCalling@users.noreply.github.com>
2023-09-10 19:59:58 +0000

    Updated twenty-one to use yaml files for messages

--- RB110/lesson-3/twenty-one/twenty_one.rb
+++ RB110/lesson-3/twenty-one/twenty_one.rb
@@ -12,8 +12,167 @@ AFFIRMATIVE = %w(y yes)
 MESSAGES = YAML.load_file('messages.yaml')
 CARD_ART = YAML.load_file('card_art.yaml')
 
-def prompt(message)
-  puts "=> #{message}"
+def busted?(entity)
+  entity[:hand_total] > MAX_HAND
+end
+
+def calculate_total(hand)
+  values = hand.map { |_suit, value| value }
+  total = 0
+
+  values.each do |value|
+    total += score_card(value)
+  end
+
+  # correct for Aces
+  values.select { |value| value == 'A' }.size.times do
+    total -= 10 if total > MAX_HAND
+  end
+
+  total
+end
+
+def create_card_end(hand)
+  num_cards = hand.size
+  string = ''
+
+  num_cards.times do
+    string += '+-------+  '
+  end
+
+  string
+end
+
+def create_card_line2(hand)
+  string = ''
+
+  hand.each do |_suit, value|
+    string += "|#{value.to_s.ljust(7)}|  "
+  end
+
+  string
+end
+
+def create_card_line7(hand)
+  string = ''
+
+  hand.each do |_suit, value|
+    string += "|#{value.to_s.rjust(7)}|  "
+  end
+
+  string
+end
+
+def create_card_middle(hand, line)
+  string = ''
+
+  hand.each do |suit, _value|
+    string += CARD_ART[suit][line]
+  end
+
+  string
+end
+
+def deal_card!(deck, number=1)
+  deck.shift(number)
+end
+
+def dealer_turn!(player, dealer, deck)
+  loop do
+    display_dealt_cards(player, dealer, 'dealer')
+    sleep(1)
+
+    dealer[:busted] = busted?(dealer)
+    break if dealer_turn_over?(dealer)
+
+    dealer[:hand] += deal_card!(deck)
+    dealer[:hand_total] = calculate_total(dealer[:hand])
+  end
+end
+
+def dealer_turn_over?(dealer)
+  dealer[:busted] || dealer[:hand_total] >= DEALER_STAY_MIN
+end
+
+def determine_winner(player, dealer)
+  if player[:busted]
+    1
+  elsif dealer[:busted]
+    2
+  elsif dealer[:hand_total] > player[:hand_total]
+    3
+  elsif player[:hand_total] > dealer[:hand_total]
+    4
+  else
+    5
+  end
+end
+
+def display_current_score(player, dealer)
+  prompt(MESSAGES['current_score'])
+  prompt("#{player[:name]}: #{player[:score]}, Dealer: #{dealer[:score]}")
+end
+
+def display_dealer_hand(hand, turn)
+  if turn == 'player'
+    hand = hand.map(&:clone)
+    hand[0] = ['unknown', '?']
+  end
+
+  display_hand(hand)
+end
+
+def display_dealt_cards(player, dealer, turn='player')
+  system('clear') || system('cls')
+  prompt('Dealer hand:')
+  display_dealer_hand(dealer[:hand], turn)
+
+  unless turn == 'player'
+    prompt("Dealer current total: #{dealer[:hand_total]}")
+  end
+
+  prompt('Your hand:')
+  display_hand(player[:hand])
+  prompt("Your current total: #{player[:hand_total]}")
+end
+
+def display_game_winner(player, dealer)
+  winner = if player[:score] == MAX_SCORE
+             player[:name]
+           elsif dealer[:score] == MAX_SCORE
+             'Dealer'
+           end
+
+  prompt("#{winner} wins the game!")
+end
+
+def display_hand(hand)
+  puts create_card_end(hand)
+  puts create_card_line2(hand)
+
+  0.upto(3) do |line|
+    puts create_card_middle(hand, line)
+  end
+
+  puts create_card_line7(hand)
+  puts create_card_end(hand)
+end
+
+def display_round_winner(player, dealer)
+  winner = determine_winner(player, dealer)
+
+  case winner
+  when 1
+    prompt("#{player[:name]} busted! Dealer wins the round!")
+  when 2
+    prompt("Dealer busted! #{player[:name]} wins the round!")
+  when 3
+    prompt("Dealer wins the round!")
+  when 4
+    prompt("#{player[:name]} wins the round!")
+  else
+    prompt("Its a tie!")
+  end
 end
 
 def game_intro
@@ -46,18 +205,42 @@ def get_player_name
   name
 end
 
-# regex Looks for a string containing only one or more whitespace characters.
+def get_player_move
+  move = ''
 
-def valid_name?(string)
-  !(string.empty? || /^\s+$/.match?(string))
+  loop do
+    prompt("Your move:")
+    prompt("Press 'h' to hit or 's' to stay")
+    move = gets.chomp.downcase
+
+    break if valid_choice?(move)
+
+    prompt('That is not a valid choice.')
+    prompt('Only h, s, hit or stay are valid choices.')
+    prompt('Please try again.')
+  end
+
+  move
+end
+
+def get_player_play_again_choice
+  answer = ''
+
+  loop do
+    prompt('Would you like to play again?  (y/n)')
+    answer = gets.chomp.downcase
+
+    break if valid_play_again_response?(answer)
+
+    prompt('That is not a valid choice. Please try again.')
+    prompt('Acceptable answers are y, n, yes, or no.')
+  end
+
+  answer
 end
 
 def initialize_deck
   SUITS.product(CARD_VALUES).shuffle
-end
-
-def deal_card!(deck, number=1)
-  deck.shift(number)
 end
 
 def initialize_round!(player, dealer, deck)
@@ -65,6 +248,18 @@ def initialize_round!(player, dealer, deck)
   dealer[:hand] += deal_card!(deck, 2)
   player[:hand_total] = calculate_total(player[:hand])
   dealer[:hand_total] = calculate_total(dealer[:hand])
+end
+
+def max_score_reached?(player, dealer)
+  player == MAX_SCORE || dealer == MAX_SCORE
+end
+
+def play_again?(answer)
+  AFFIRMATIVE.include?(answer)
+end
+
+def player_stays?(string)
+  STAY.include?(string)
 end
 
 def player_turn!(player, dealer, deck)
@@ -85,203 +280,8 @@ def player_turn!(player, dealer, deck)
   end
 end
 
-def display_dealt_cards(player, dealer, turn='player')
-  system('clear') || system('cls')
-  prompt('Dealer hand:')
-  display_dealer_hand(dealer[:hand], turn)
-
-  unless turn == 'player'
-    prompt("Dealer current total: #{dealer[:hand_total]}")
-  end
-
-  prompt('Your hand:')
-  display_hand(player[:hand])
-  prompt("Your current total: #{player[:hand_total]}")
-end
-
-def display_dealer_hand(hand, turn)
-  if turn == 'player'
-    hand = hand.map(&:clone)
-    hand[0] = ['unknown', '?']
-  end
-
-  display_hand(hand)
-end
-
-def display_hand(hand)
-  puts create_card_end(hand)
-  puts create_card_line2(hand)
-
-  0.upto(3) do |line|
-    puts create_card_middle(hand, line)
-  end
-
-  puts create_card_line7(hand)
-  puts create_card_end(hand)
-end
-
-def create_card_end(hand)
-  num_cards = hand.size
-  string = ''
-
-  num_cards.times do
-    string += '+-------+  '
-  end
-
-  string
-end
-
-def create_card_line2(hand)
-  string = ''
-
-  hand.each do |_suit, value|
-    string += "|#{value.to_s.ljust(7)}|  "
-  end
-
-  string
-end
-
-def create_card_middle(hand, line)
-  string = ''
-
-  hand.each do |suit, _value|
-    string += CARD_ART[suit][line]
-  end
-
-  string
-end
-
-def create_card_line7(hand)
-  string = ''
-
-  hand.each do |_suit, value|
-    string += "|#{value.to_s.rjust(7)}|  "
-  end
-
-  string
-end
-
-def busted?(entity)
-  entity[:hand_total] > MAX_HAND
-end
-
-def get_player_move
-  move = ''
-
-  loop do
-    prompt("Your move:")
-    prompt("Press 'h' to hit or 's' to stay")
-    move = gets.chomp.downcase
-
-    break if valid_choice?(move)
-
-    prompt('That is not a valid choice.')
-    prompt('Only h, s, hit or stay are valid choices.')
-    prompt('Please try again.')
-  end
-
-  move
-end
-
-def valid_choice?(string)
-  MOVES.include?(string)
-end
-
-def player_stays?(string)
-  STAY.include?(string)
-end
-
-def calculate_total(hand)
-  values = hand.map { |_suit, value| value }
-  total = 0
-
-  values.each do |value|
-    total += score_card(value)
-  end
-
-  # correct for Aces
-  values.select { |value| value == 'A' }.size.times do
-    total -= 10 if total > MAX_HAND
-  end
-
-  total
-end
-
-def score_card(card)
-  case card
-  when 'J', 'Q', 'K'
-    10
-  when 'A'
-    11
-  else
-    card
-  end
-end
-
-def dealer_turn!(player, dealer, deck)
-  loop do
-    display_dealt_cards(player, dealer, 'dealer')
-    sleep(1)
-
-    dealer[:busted] = busted?(dealer)
-    break if dealer_turn_over?(dealer)
-
-    dealer[:hand] += deal_card!(deck)
-    dealer[:hand_total] = calculate_total(dealer[:hand])
-  end
-end
-
-def dealer_turn_over?(dealer)
-  dealer[:busted] || dealer[:hand_total] >= DEALER_STAY_MIN
-end
-
-def display_round_winner(player, dealer)
-  winner = determine_winner(player, dealer)
-
-  case winner
-  when 1
-    prompt("#{player[:name]} busted! Dealer wins the round!")
-  when 2
-    prompt("Dealer busted! #{player[:name]} wins the round!")
-  when 3
-    prompt("Dealer wins the round!")
-  when 4
-    prompt("#{player[:name]} wins the round!")
-  else
-    prompt("Its a tie!")
-  end
-end
-
-def determine_winner(player, dealer)
-  if player[:busted]
-    1
-  elsif dealer[:busted]
-    2
-  elsif dealer[:hand_total] > player[:hand_total]
-    3
-  elsif player[:hand_total] > dealer[:hand_total]
-    4
-  else
-    5
-  end
-end
-
-def score_round!(player, dealer)
-  winner = determine_winner(player, dealer)
-
-  case winner
-  when 1, 3 then dealer[:score] += 1
-  when 2, 4 then player[:score] += 1
-  end
-end
-
-def display_current_score(player, dealer)
-  prompt('Current score is...')
-  prompt("#{player[:name]}: #{player[:score]}, Dealer: #{dealer[:score]}")
-end
-
-def max_score_reached?(player, dealer)
-  player == MAX_SCORE || dealer == MAX_SCORE
+def prompt(message)
+  puts "=> #{message}"
 end
 
 def round_reset!(player, dealer)
@@ -296,39 +296,41 @@ def round_reset!(player, dealer)
   gets
 end
 
-def display_game_winner(player, dealer)
-  winner = if player[:score] == MAX_SCORE
-             player[:name]
-           elsif dealer[:score] == MAX_SCORE
-             'Dealer'
-           end
-
-  prompt("#{winner} wins the game!")
+def score_card(card)
+  case card
+  when 'J', 'Q', 'K'
+    10
+  when 'A'
+    11
+  else
+    card
+  end
 end
 
-def get_player_play_again_choice
-  answer = ''
+def score_round!(player, dealer)
+  winner = determine_winner(player, dealer)
 
-  loop do
-    prompt('Would you like to play again?  (y/n)')
-    answer = gets.chomp.downcase
-
-    break if valid_play_again_response?(answer)
-
-    prompt('That is not a valid choice. Please try again.')
-    prompt('Acceptable answers are y, n, yes, or no.')
+  case winner
+  when 1, 3 then dealer[:score] += 1
+  when 2, 4 then player[:score] += 1
   end
+end
 
-  answer
+def valid_choice?(string)
+  MOVES.include?(string)
+end
+
+# regex looks for a string containing only one or more whitespace characters.
+
+def valid_name?(string)
+  !(string.empty? || /^\s+$/.match?(string))
 end
 
 def valid_play_again_response?(string)
   PLAYER_CHOICES.include?(string)
 end
 
-def play_again?(answer)
-  AFFIRMATIVE.include?(answer)
-end
+
 
 # Main
 
